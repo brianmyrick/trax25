e1042e983facc93b4269cc655f958528905e8ad2
brianmyrick <brianm@interversant.com>
2025-06-12 13:17:39 -0400

    Initial Commit

---
+++ trax25/trax25App.swift
@@ -0,0 +1,17 @@
+//
+//  trax25App.swift
+//  trax25
+//
+//  Created by Brian Myrick on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct trax25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ trax25Tests/trax25Tests.swift
@@ -0,0 +1,17 @@
+//
+//  trax25Tests.swift
+//  trax25Tests
+//
+//  Created by Brian Myrick on 6/12/25.
+//
+
+import Testing
+@testable import trax25
+
+struct trax25Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
